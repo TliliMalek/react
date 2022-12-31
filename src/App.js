cbfa610522167bb92d361f7332712406fe2cc672
malek <tlililmalek@gmail.com>
2022-12-31 15:26:32 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,116 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Badge from 'react-bootstrap/Badge';
+import Carousel from 'react-bootstrap/Carousel';
+
+
+function BasicExample() {
+  return (
+   <div className='hello'>
+    <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Recrutement</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">About</Nav.Link>
+            <Nav.Link href="#pricing">Resume</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+      
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+    <Badge pill bg="primary">
+        Primary
+      </Badge>{' '}
+      <Badge pill bg="secondary">
+        Secondary
+      </Badge>{' '}
+      <Badge pill bg="success">
+        Success
+      </Badge>{' '}
+      <Badge pill bg="danger">
+        Danger
+      </Badge>{' '}
+      <Badge pill bg="warning" text="dark">
+        Warning
+      </Badge>{' '}
+      <Badge pill bg="info">
+        Info
+      </Badge>{' '}
+      <Badge pill bg="light" text="dark">
+        Light
+      </Badge>{' '}
+      <Badge pill bg="dark">
+        Dark
+      </Badge>
+      <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://i.pinimg.com/564x/3f/bc/e8/3fbce8fcb376a4cf6f88c534d34634e4.jpg" height="500"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://i.pinimg.com/564x/68/50/81/685081bfa1e07cc877c10be9af52c3eb.jpg" height="500" width="100"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://i.pinimg.com/564x/82/53/6a/82536a031b6195e4944efe058cf3b2a1.jpg" height="500" width="0.002"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+   
+    </div>
+  );
+}
+
+
+export default BasicExample;
